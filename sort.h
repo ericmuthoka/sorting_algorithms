6fd5fe904c032a0da249e03fa4317d3ff37294e0
ericmuthoka <muthokaerick@gmail.com>
2023-07-24 04:09:27 -0700

    sorting algorithms

--- sort.h
+++ sort.h
@@ -19,32 +19,6 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct card_s - Playing card
- *
- * @value: Value of the card
- * From "Ace" to "King"
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-	const char *value;
-	const kind_t kind;
-} card_t;
-
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
-} deck_node_t;
-/**
  * enum bool - Boolean values enumeration
  * @false: is 0.
  * @true: is 1.
@@ -69,6 +43,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif
